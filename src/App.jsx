45a6398a78478cec5a740424702c0f5d5154aea1
Techie <gamevalorant149@gamil.com>
2026-01-22 23:04:30 +0530

    adding forgot page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/homepage/HomePage";
 import LoginPage from "./components/auth/LoginPage";
+import ForgotPasswordPage from "./components/auth/ForgotPasswordPage";
+
 
 const App = () => (
   <BrowserRouter>
@@ -8,6 +10,7 @@ const App = () => (
       <Route path="/" element={<HomePage />} />
       <Route path="/homepage" element={<HomePage />} />
       <Route path="/login" element = {<LoginPage/>}/>
+      <Route path="/forgot" element = {<ForgotPasswordPage/>}/>
     </Routes>
   </BrowserRouter>
 );
--- src/api/auth.js
+++ src/api/auth.js
@@ -1,10 +1,13 @@
-import api from './index.js';
+import api from "./index.js";
 
-export const registerApi = async (userData)=>{
-    return api.post('/auth/register', userData);
-}
+export const registerApi = async (userData) => {
+  return api.post("/auth/register", userData);
+};
 
-export const loginApi = async (loginData)=>{
-    return api.post('/auth/login', loginData);
-}
+export const loginApi = async (loginData) => {
+  return api.post("/auth/login", loginData);
+};
 
+export const changePasswordApi = async (forgotData) => {
+  return api.post("/auth/change-password", forgotData);
+};
